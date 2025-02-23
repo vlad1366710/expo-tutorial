80033717ae0b5232337ec86e6e4fd7cdc64000d8
Perceval <vlad.psu@mail.ru>
2025-02-23 17:34:24 +0300

    Настройте строку состояния, заставку и значок приложения

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,20 +1,16 @@
 import { Stack } from 'expo-router';
 
-// Экспортируем компонент RootLayout по умолчанию
-// Этот компонент определяет структуру навигации приложения
+import { StatusBar } from 'expo-status-bar';
+
+
 export default function RootLayout() {
   return (
-    // Используем компонент Stack для создания стека навигации
-    <Stack>
-
-    {/* 
-        Stack.Screen — это компонент, который определяет отдельный экран в стеке навигации.
-        - Атрибут `name` указывает имя экрана (должно совпадать с именем файла в папке `app`).
-        - Атрибут `options` позволяет настроить отображение экрана (например, заголовок).
-      */}
-
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
